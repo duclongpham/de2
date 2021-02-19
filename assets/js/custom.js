b7bf509bc39a3581175bb55ff10083990207af4a
longphamduc <phamduclong2000192@gmail.com>
2021-02-19 13:08:07 +0700

    fix bug

---
+++ assets/js/custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.slider__list').slick({
+        nextArrow: $('.slider__next'),
+        prevArrow: $('.slider__prev')
+    });
+    $('.top-selling__list').slick({
+        nextArrow: $('.top-selling__title--next'),
+        prevArrow: $('.top-selling__title--prev'),
+    });
+
+    $('.menu__left--icon').on('click', function() {
+        $('.menu__right').css({ display: 'block' });
+
+    })
+    $('.menu__right--bg').on('click', function() {
+        $('.menu__right').removeAttr('style');
+    });
+})
